8550aa823f1b88744298909cf6b06c92855c3828
Lucas <lucaswoodyard@yahoo.com>
2022-12-12 15:27:10 -0600

    Added a h1 element as well as a dark mode button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import React from "react";
 function Header() {
     return (
         <div>
-            
+            <h1 class="title">Luke's Favorite Movies</h1>
+            <button class="darkmode">Dark Mode</button>
         </div>
     )
 }
