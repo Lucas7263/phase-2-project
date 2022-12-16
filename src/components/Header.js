1dcec8fe341d4ecfbd227fa78a86ebda104862e6
Lucas <lucaswoodyard@yahoo.com>
2022-12-16 16:40:43 -0600

    Added a useEffect clock function with additional CSS for the clock.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function Header() {
+  
+
+      
+    function Clock() {
+        const [time, setTime] = useState(new Date());
+      
+        useEffect(() => {
+          setInterval(() => {
+            setTime(new Date());
+          }, 1000);
+        }, []);
+      
+        return <div>{time.toString()}</div>;
+      }
+    
+
     return (
         <div>
             <h1 class="title">Luke's Favorite Movies</h1>
             <button class="darkmode">Dark Mode</button>
-            <h3>Favorite Movie List</h3>
+            <div class="time">{Clock()}</div>
+           
         </div>
     )
 }
