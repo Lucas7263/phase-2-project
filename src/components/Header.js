4fb12f95d7d0e55f4b5174cd4730814435d27e14
Lucas <lucaswoodyard@yahoo.com>
2022-12-16 18:26:54 -0600

    Added favorite movies to the db.json file.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,7 @@ import React, {useEffect, useState} from "react";
 
 function Header() {
   
-
-      
-    function Clock() {
+ function Clock() {
         const [time, setTime] = useState(new Date());
       
         useEffect(() => {
