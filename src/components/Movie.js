8b03dcce61c9a263d1c894f4ae2c4c8e757180f8
Lucas <lucaswoodyard@yahoo.com>
2022-12-18 19:34:35 -0600

    refactored code in the movieList component.

---
+++ src/components/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Movielist from './MovieList';
+
+
+function Movie(movie) {
+    const {title, image, about, id} = movie
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Movie
+
+
+// this component will build out each individula movie to display on the page.
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Movie from './Movie';
 
 
-function Movielist() {
+function Movielist({movieData}) {
     console.log()
+   const movies = movieData.map(movie => {
+          return (<Movie movie={movie}/>
+          )
+   })  
     return (
         <div>
-            
+            {movies}
         </div>
        
     )
 }
 
 export default Movielist
+
+
+//make new movie compenent for each movie
+//use movielist component to map through data and destructure
